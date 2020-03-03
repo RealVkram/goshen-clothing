73a633f096c82c77fa36b249aacde737203b3994
RealVkram <ajayiadegbemi@gmail.com>
2020-03-03 13:52:47 +0100

    split the homepage into smaller components and stlye menu item components

---
+++ src/pages/homepage/homepage.components.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './homepage.styles.scss';
+import DirectoryMenu from '../../components/directory/Directory.components'
+
+const Homepage = () => (
+    <DirectoryMenu/>
+)
+    
+export default Homepage;
